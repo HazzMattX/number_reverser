b7cc4e2ec217db9c2c08b30897c6c038d97e4d5d
HazzMattX <dowdmatt93@gmail.com>
2025-04-03 13:03:00 -0700

    add initial build artifacts and configuration for number_reverser

---
+++ target/debug/deps/number_reverser-90ccc47de8d87d46.d
@@ -0,0 +1,5 @@
+/Users/matthewdowd/GitHub/rust-projects/number_reverser/target/debug/deps/libnumber_reverser-90ccc47de8d87d46.rmeta: src/main.rs
+
+/Users/matthewdowd/GitHub/rust-projects/number_reverser/target/debug/deps/number_reverser-90ccc47de8d87d46.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/number_reverser-9f11258731eaf26b.d
@@ -0,0 +1,5 @@
+/Users/matthewdowd/GitHub/rust-projects/number_reverser/target/debug/deps/libnumber_reverser-9f11258731eaf26b.rmeta: src/main.rs
+
+/Users/matthewdowd/GitHub/rust-projects/number_reverser/target/debug/deps/number_reverser-9f11258731eaf26b.d: src/main.rs
+
+src/main.rs:
